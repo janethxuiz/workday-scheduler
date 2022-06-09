2f331cc552d76280278696fe4346287a3b3dcf22
janethxuiz <janethruiz152@gmail.com>
2022-06-08 23:28:49 -0700

    adding context to my README file

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var time = $(this)
 .parent()
 .attr("id");
 
+//setInterval(updateDate, 1000);
+
 localStorage.setItem(time, value);
 
     });
@@ -35,9 +37,23 @@ function updateHour() {
     });
 }
 
+function updateDate() {
+    $("#date").text(moment().format("dddd, MMMM Do - h:mm:ss a"));
+}
+
+
 updateHour();
 
 $("#hour-9 .discription").val(localStorage.getItem("hour-9"));
+$("#hour-10 .discription").val(localStorage.getItem("hour-10"));
+$("#hour-11 .discription").val(localStorage.getItem("hour-11"));
+$("#hour-12 .discription").val(localStorage.getItem("hour-12"));
+$("#hour-13 .discription").val(localStorage.getItem("hour-13"));
+$("#hour-14 .discription").val(localStorage.getItem("hour-14"));
+$("#hour-15 .discription").val(localStorage.getItem("hour-15"));
+$("#hour-16 .discription").val(localStorage.getItem("hour-16"));
+$("#hour-17 .discription").val(localStorage.getItem("hour-17"));
+
 
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
