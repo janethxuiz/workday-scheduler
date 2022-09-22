7c58329149c6beac1b225df3f5683f6e2e4be971
janethxuiz <janethruiz152@gmail.com>
2022-09-22 01:13:32 -0700

    fixed dates format, as well as changed some of the js format

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
+var todaysDate = moment().format("dddd, MMM Do YYYY");
+$("#currentDay").html(todaysDate);
+
 $(document).ready(function() {
-    $(".save-btn").on("click", function(){
-var value = $(this)
-.siblings(".discription")
-.val();
-var time = $(this)
-.parent()
-.attr("id");
+    $(".save-btn").on("click", function() {
+
+var text = $(this).siblings(".discription").val();
+var time = $(this).parent().attr("id");
 
 //setInterval(updateDate, 1000);
+localStorage.setItem(time, text);
 
-localStorage.setItem(time, value);
-
-    });
-
-
+})
 
 function updateHour() {
     var currentTime = moment().hours();
+
     $(".block-time").each(function(){
-        var hourBlock = parseInt(
-            $(this)
-            .attr("id")
-            .split("-")[1]
+        var hourBlock = parseInt($(this).attr("id").split("hour")[1]
         );
         
         if (hourBlock < currentTime){
             $(this).addClass("past");
-        } else if (hourBlock === currentTime){
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (hourBlock === currentTime){
             $(this).removeClass("past");
+            $(this).removeClass("future");
             $(this).addClass("present");
-        } else{
+        }
+        else{
             $(this).removeClass("past");
             $(this).removeClass("present");
             $(this).addClass("future");
         }
-    });
+    })
 }
 
-function updateDate() {
-    $("#date").text(moment().format("dddd, MMMM Do - h:mm:ss a"));
-}
-
-
-updateHour();
+// function updateDate() {
+//     $("#date").text(moment().format("dddd, MMMM Do - h:mm:ss a"));
+// }
 
 $("#hour-9 .discription").val(localStorage.getItem("hour-9"));
 $("#hour-10 .discription").val(localStorage.getItem("hour-10"));
@@ -54,7 +51,7 @@ $("#hour-15 .discription").val(localStorage.getItem("hour-15"));
 $("#hour-16 .discription").val(localStorage.getItem("hour-16"));
 $("#hour-17 .discription").val(localStorage.getItem("hour-17"));
 
+updateHour();
+// $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
-
-});
+})
